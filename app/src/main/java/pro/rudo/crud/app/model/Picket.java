64f007c1420504f6bf4f30ed32330a3fe67107cf
Rudolf <rudocris@gmail.com>
2014-05-03 01:36:13 +0400

    Добавил класс Пикет и Билдер для Пикета

---
+++ app/src/main/java/pro/rudo/crud/app/model/Picket.java
@@ -0,0 +1,180 @@
+package pro.rudo.crud.app.model;
+
+/**
+ * Created by rudolf on 03.05.14.
+ */
+public class Picket {
+    private boolean isRay = false;
+    private String from;
+    private String to;
+    private double length;
+    private double azimuth;
+    private double incline;
+    private double backAzimuth;
+    private double backIncline;
+    private double left;
+    private String leftComment;
+    private double right;
+    private String rightComment;
+    private double up;
+    private String upComment;
+    private double down;
+    private String downComment;
+    private String comment;
+
+    public Picket(PicketBuilder builder){
+        this.isRay = builder.isRay();
+        this.from = builder.getFrom();
+        this.to = builder.getTo();
+        this.azimuth = builder.getAzimuth();
+        this.backAzimuth = builder.getBackAzimuth();
+        this.incline = builder.getIncline();
+        this.backIncline = builder.getBackIncline();
+        this.left = builder.getLeft();
+        this.leftComment = builder.getLeftComment();
+        this.right = builder.getRight();
+        this.rightComment = builder.getRightComment();
+        this.up = builder.getUp();
+        this.upComment = builder.getUpComment();
+        this.down = builder.getDown();
+        this.downComment = builder.getDownComment();
+        this.comment = builder.getComment();
+    }
+
+    public String getLeftComment() {
+        return leftComment;
+    }
+
+    public void setLeftComment(String leftComment) {
+        this.leftComment = leftComment;
+    }
+
+    public String getRightComment() {
+        return rightComment;
+    }
+
+    public void setRightComment(String rightComment) {
+        this.rightComment = rightComment;
+    }
+
+    public String getUpComment() {
+        return upComment;
+    }
+
+    public void setUpComment(String upComment) {
+        this.upComment = upComment;
+    }
+
+    public String getDownComment() {
+        return downComment;
+    }
+
+    public void setDownComment(String downComment) {
+        this.downComment = downComment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public boolean isRay() {
+        return isRay;
+    }
+
+    public void setRay(boolean isRay) {
+        this.isRay = isRay;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getAzimuth() {
+        return azimuth;
+    }
+
+    public void setAzimuth(double azimuth) {
+        this.azimuth = azimuth;
+    }
+
+    public double getIncline() {
+        return incline;
+    }
+
+    public void setIncline(double incline) {
+        this.incline = incline;
+    }
+
+    public double getBackAzimuth() {
+        return backAzimuth;
+    }
+
+    public void setBackAzimuth(double backAzimuth) {
+        this.backAzimuth = backAzimuth;
+    }
+
+    public double getBackIncline() {
+        return backIncline;
+    }
+
+    public void setBackIncline(double backIncline) {
+        this.backIncline = backIncline;
+    }
+
+    public double getLeft() {
+        return left;
+    }
+
+    public void setLeft(double left) {
+        this.left = left;
+    }
+
+    public double getRight() {
+        return right;
+    }
+
+    public void setRight(double right) {
+        this.right = right;
+    }
+
+    public double getUp() {
+        return up;
+    }
+
+    public void setUp(double up) {
+        this.up = up;
+    }
+
+    public double getDown() {
+        return down;
+    }
+
+    public void setDown(double down) {
+        this.down = down;
+    }
+
+}
---
+++ app/src/main/java/pro/rudo/crud/app/model/PicketBuilder.java
@@ -0,0 +1,179 @@
+package pro.rudo.crud.app.model;
+
+public class PicketBuilder {
+    public Picket createPicket() {
+        return new Picket(this);
+    }
+
+    private boolean isRay = false;
+    private String from;
+    private String to;
+    private double length;
+    private double azimuth;
+    private double incline;
+    private double backAzimuth;
+    private double backIncline;
+    private double left;
+    private String leftComment;
+    private double right;
+
+    public String getLeftComment() {
+        return leftComment;
+    }
+
+    public String getRightComment() {
+        return rightComment;
+    }
+
+    public String getUpComment() {
+        return upComment;
+    }
+
+    public String getDownComment() {
+        return downComment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    private String rightComment;
+    private double up;
+    private String upComment;
+    private double down;
+    private String downComment;
+    private String comment;
+
+    public boolean isRay() {
+        return isRay;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getAzimuth() {
+        return azimuth;
+    }
+
+    public double getIncline() {
+        return incline;
+    }
+
+    public double getBackAzimuth() {
+        return backAzimuth;
+    }
+
+    public double getBackIncline() {
+        return backIncline;
+    }
+
+    public double getLeft() {
+        return left;
+    }
+
+    public double getRight() {
+        return right;
+    }
+
+    public double getUp() {
+        return up;
+    }
+
+    public double getDown() {
+        return down;
+    }
+
+    public PicketBuilder withRay(boolean ray) {
+        this.isRay = ray;
+        return this;
+    }
+
+    public PicketBuilder withFrom(String from) {
+        this.from = from;
+        return this;
+    }
+
+    public PicketBuilder withTo(String to) {
+        this.to = to;
+        return this;
+    }
+
+    public PicketBuilder withLength(double length) {
+        this.length = length;
+        return this;
+    }
+
+    public PicketBuilder withAzimuth(double azimuth) {
+        this.azimuth = azimuth;
+        return this;
+    }
+
+    public PicketBuilder withIncline(double incline) {
+        this.incline = incline;
+        return this;
+    }
+
+    public PicketBuilder withBackAzimuth(double azimuth) {
+        this.backAzimuth = azimuth;
+        return this;
+    }
+
+    public PicketBuilder withBackIncline(double incline) {
+        this.backIncline = incline;
+        return this;
+    }
+
+    public PicketBuilder withLeft(double left) {
+        this.left = left;
+        return this;
+    }
+
+    public PicketBuilder withLeftComment(String comment) {
+        this.leftComment = comment;
+        return this;
+    }
+
+    public PicketBuilder withRight(double right) {
+        this.right = right;
+        return this;
+    }
+
+    public PicketBuilder withRightComment(String comment) {
+        this.rightComment = comment;
+        return this;
+    }
+
+    public PicketBuilder withUp(double up) {
+        this.up = up;
+        return this;
+    }
+
+    public PicketBuilder withUpComment(String comment) {
+        this.upComment = comment;
+        return this;
+    }
+
+    public PicketBuilder withDown(double down) {
+        this.down = down;
+        return this;
+    }
+
+    public PicketBuilder withDownComment(String comment) {
+        this.downComment = comment;
+        return this;
+    }
+
+    public PicketBuilder withComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+}
